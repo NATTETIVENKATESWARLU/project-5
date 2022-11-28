2a44526047bd147d3dea625d4afa0767ca6bdd56
NATTETIVENKATESWARLU <n.venkateswarlu180@gmail.com>
2022-11-28 21:19:49 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def a1_first(request):
+    return render(request,'a1_first.html')
+    
+
+# Create your views here.
+def a1_second(request):
+    return render(request,'a1_second.html')
+    
